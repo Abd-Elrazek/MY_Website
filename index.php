433e2d646e15145b1f0302ae29cd2ef8b1eb1785
Abd-Elrazek <abdelrazek.n@yahoo.com>
2018-07-20 22:44:26 +0200

    this initial files for this website

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <title> Abdelrazek </title>
+    <meta charset="UTF-8" />
+    <meta content="Contact Us" name="description" />
+    <meta content="Contact Us " name="twitter:description" />
+    <meta content="Contact Us " property="cg:description" />
+    <!-- For Internet Explorer -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- for Device mobile -->
+    <meta name="Content" content="width=device-width,initial-scale=1">
+    <link rel="shortcut icon" href="favicon.ico" />
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="css/fontawesome-all.css" type="text/css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=BioRhyme+Expanded:400,700">
+    <link rel="stylesheet" href="css/main-style.css" />
+
+
+</head>
+
+<body>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only"> </span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a class="h" href="#">Home <span class="sr-only">(current)</span></a></li>
+                    <li><a class="h" href="#">About Me</a></li>
+                    <li><a class="h" href="#">Skills</a></li>
+                    <li><a class="h" href="#">Services</a></li>
+                    <li><a class="h" href="#">Contact</a></li>
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <div class="container">
+
+    </div>
+
+
+
+    <!-- basic js load..... -->
+    <script src=" js/jquery-3.2.1.min.js "></script>
+    <script src="js/bootstrap.min.js "></script>
+
+
+</body>
+
+</html>
