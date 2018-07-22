65d19ce84136787f8efe1122ebc248a7bad5a926
Abd-Elrazek <abdelrazek.n@yahoo.com>
2018-07-22 07:43:05 +0200

    nothing

--- index.php
+++ index.php
@@ -3,135 +3,140 @@
 <html lang="en">
 
 <head>
-    <title> Abdelrazek </title>
-    <meta charset="UTF-8" />
-    <meta content="Contact Us" name="description" />
-    <meta content="Contact Us " name="twitter:description" />
-    <meta content="Contact Us " property="cg:description" />
-    <!-- For Internet Explorer -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <!-- for Device mobile -->
-    <meta name="Content" content="width=device-width,initial-scale=1">
-    <link rel="shortcut icon" href="favicon.ico" />
-    <link rel="stylesheet" href="css/bootstrap.css" />
-    <link rel="stylesheet" href="css/fontawesome-all.css" type="text/css" />
-    <link rel="stylesheet" href="css/font-google.css" />
-    <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Marck+Script|Monoton|Sacramento|Source+Code+Pro" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Nixie+One" rel="stylesheet">
-    <link rel="stylesheet" href="css/main-style.css" />
-    <link rel="stylesheet" href="css/toggle-switch.css" />
-    <style type="text/css">
-        a:hover {
-            color: blue important;
-        }
+	<title> Abdelrazek </title>
+	<meta charset="UTF-8" />
+	<meta content="Contact Us" name="description" />
+	<meta content="Contact Us " name="twitter:description" />
+	<meta content="Contact Us " property="cg:description" />
+	<!-- For Internet Explorer -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<!-- for Device mobile -->
+	<meta name="Content" content="width=device-width,initial-scale=1">
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" href="css/bootstrap.css" />
+	<link rel="stylesheet" href="css/fontawesome-all.css" type="text/css" />
+	<link rel="stylesheet" href="css/font-google.css" />
+	<link href="https://fonts.googleapis.com/css?family=Indie+Flower|Marck+Script|Monoton|Sacramento|Source+Code+Pro" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Nixie+One" rel="stylesheet">
+	<link rel="stylesheet" href="css/main-style.css" />
+	<link rel="stylesheet" href="css/toggle-switch.css" />
+	<style type="text/css">
+		a:hover {
+			color: blue important;
+		}
 
-    </style>
+	</style>
 </head>
 
 <body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only"> </span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-            </div>
+			</div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a class="h" href="#">Home <span class="sr-only">(current)</span></a></li>
-                    <li><a class="h" href="#">About Me</a></li>
-                    <li><a class="h" href="#">Skills</a></li>
-                    <li><a class="h" href="#">Services</a></li>
-                    <li><a class="h" href="#">Contact</a></li>
-                </ul>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav list_right">
+					<li class="active"><a class="h" href="#">Home <span class="sr-only">(current)</span></a></li>
+					<li><a class="h text-center" href="#">About Me</a></li>
+					<li><a class="h text-center" href="#">Skills</a></li>
+					<li><a class="h text-center" href="#">Services</a></li>
+					<li><a class="h text-center" href="#">Contact</a></li>
+				</ul>
 
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <div class="main_switch">
-                            <i class="fas fa-moon fa-2x"></i>
-                            <input type="checkbox" id="switch" />
-                            <label class="T_" for="switch">Switch</label>
-                            <i class="fas sun_icon fa-sun fa-2x"></i>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container-fluid -->
-    </nav>
-    <!--  start home page  -->
-    <div class="card home">
-        <div class="inside_card">
-            <div class="row">
-                <h2 class="text-center">Home</h2>
-                <p class="text-center lead">An aspiring Software Engineer who is in a journey to become a full-stack developer.</p>
-                <div class="col-sm-6  col-xs-12">
-                    <div class="home_image">
-                        <img class="img-circle" src="images/image.png" title="Developer : ^__^ abdelrazek ^__^" alt="image not found, internet erorr">
-                    </div>
-                </div>
+				<ul class="nav navbar-nav navbar-right">
+					<li>
+						<div class="main_switch">
+							<i class="fas fa-moon fa-2x"></i>
+							<input type="checkbox" id="switch" />
+							<label class="T_" for="switch">Switch</label>
+							<i class="fas sun_icon fa-sun fa-2x"></i>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+		<!-- /.container-fluid -->
+	</nav>
+	<!--  start home page  -->
+	<div class="card home">
+		<div class="inside_card">
+			<div class="row">
+				<h2 class="text-center">Home</h2>
+				<p class="text-center lead">An aspiring Software Engineer who is in a journey to become a full-stack developer.</p>
+				<div class="col-sm-6  col-xs-12">
+					<div class="home_image">
+						<img class="img-circle" src="images/image.png" title="Developer : ^__^ abdelrazek ^__^" alt="image not found, internet erorr">
+					</div>
+				</div>
 
-                <div class="col-sm-6  col-xs-12">
-                    <div class="home_info">
-                        <ul class="list-unstyled">
-                            <li class="list_info">
-                                <span class="lead el_info_right"> Name : </span>
-                                <span class="lead el_info">Abdelrazek Nageh</span>
-                            </li>
-                            <li class="list_info">
-                                <span class="lead el_info_right"> Email :</span>
-                                <span class="lead el_info">Abdelrazek.n3@gmail</span>
-                            </li>
-                            <li class="list_info">
-                                <span class="lead el_info_right"> Phone :</span>
-                                <span class="lead el_info">01090802147</span>
-                            </li>
-                            <li class="list_info">
-                                <span class="lead el_info_right"> Name  :</span>
-                                <span class="lead el_info">Abdelrazek Nageh</span>
-                            </li>
-                            <li class="list_info">
-                                <span class="lead el_info_f">I'm a freelancer Web . </span>
+				<div class="col-sm-6  col-xs-12">
+					<div class="home_info">
+						<ul class="list-unstyled">
+							<li class="list_info">
+								<i class="fas fa-square fa-1x"></i>
+								<span class="lead el_info_right"> Name  </span>
+								<span class="lead el_info">Abdelrazek Nageh</span>
+							</li>
+							<li class="list_info">
+								<i class="fas fa-square fa-1x"></i>
+								<span class="lead el_info_right"> Email </span>
+								<span class="lead el_info">Abdelrazek.n3@gmail.com</span>
+							</li>
+							<li class="list_info">
+								<i class="fas fa-square fa-1x"></i>
+								<span class="lead el_info_right"> Phone </span>
+								<span class="lead el_info">01090802147</span>
+							</li>
+							<li class="list_info">
+								<i class="fas fa-square fa-1x"></i>
+								<span class="lead el_info_right"> Name  </span>
+								<span class="lead el_info">Abdelrazek Nageh</span>
+							</li>
+							<li class="list_info">
+								<i class="fas fa-square fa-1x"></i>
+								<span class="lead el_info_f">I'm a freelancer Web &emsp; <i  class="fas fa-smile-beam"></i> &ensp;. </span>
 
-                            </li>
-
-
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--  start About me  page  -->
-    <div class="card">
-        dsfdsdsfdsf
-    </div>
-    <!--  start skills  page  -->
-    <div class="card">
-        dsfdsfsdfdsff
-    </div>
-    <!--  start Sevices page  -->
-    <div class="card">
-        fdsfdsfdsf
-    </div>
-    <!--  start Contact page  -->
-    <div class="card">
-        fdsfsdf
-    </div>
+							</li>
 
 
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--  start About me  page  -->
+	<div class="card">
+		dsfdsdsfdsf
+	</div>
+	<!--  start skills  page  -->
+	<div class="card">
+		dsfdsfsdfdsff
+	</div>
+	<!--  start Sevices page  -->
+	<div class="card">
+		fdsfdsfdsf
+	</div>
+	<!--  start Contact page  -->
+	<div class="card">
+		fdsfsdf
+	</div>
 
-    <!-- basic js load..... -->
-    <script src=" js/jquery-3.2.1.min.js "></script>
-    <script src="js/bootstrap.min.js "></script>
-    <script src="js/toggle-switch.js"></script>
+
+
+	<!-- basic js load..... -->
+	<script src=" js/jquery-3.2.1.min.js "></script>
+	<script src="js/bootstrap.min.js "></script>
+	<script src="js/toggle-switch.js"></script>
 
 
 </body>
