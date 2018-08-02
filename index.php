7db4ed93b7fd51bd8a354e00760571ccf13c2537
Abd-Elrazek <abdelrazek.n3@gmail.com>
2018-08-02 07:01:39 +0200

    I added Brand 'Abdelrazek'

--- index.php
+++ index.php
@@ -37,11 +37,12 @@
 			<!-- Brand and toggle get grouped for better mobile display -->
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only"> </span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
+                   <span class="sr-only"> </span>
+                   <span class="icon-bar"></span>
+                   <span class="icon-bar"></span>
+                   <span class="icon-bar"></span>
+                </button>
+				<a class="navbar-brand" href="#">Abdelrazek</a>
 			</div>
 
 			<!-- Collect the nav links, forms, and other content for toggling -->
